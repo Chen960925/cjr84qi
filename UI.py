a1facedee6a53b35351a7905f95002fe89dde92d
Chen960925 <599022161@qq.com>
2021-08-31 00:22:32 +0800

    更新

--- UI.py
+++ UI.py
@@ -1,25 +1,25 @@
 from selenium import webdriver
 import time
 import requests
-# driver=webdriver.Chrome()#选择Chorme浏览器 -- 跟浏览器建立会话==赋值给drive
-# driver.implicitly_wait(10)#隐式等待==10s,如果元素提前出现，提前结束等待：10s后还没有出线，报错！==整个会话生效
-# #1、打开具体的网址
-# driver.get("http://baidu.com")
-# #2、窗口最大化
-# driver.maximize_window()
-# #3、前进 后退 刷新
-# driver.get("http://taobao.com")
-# time.sleep(2) #等待=2s
-# driver.forward()
-# time.sleep(2)
-# driver.back()
-# time.sleep(2)
-# driver.refresh()
-# #4、关闭浏览器
-# driver.close()#关闭当前的窗口
-# driver.quit()#关闭整个浏览器
+driver=webdriver.Chrome()#选择Chorme浏览器 -- 跟浏览器建立会话==赋值给drive
+driver.implicitly_wait(10)#隐式等待==10s,如果元素提前出现，提前结束等待：10s后还没有出线，报错！==整个会话生效
+#1、打开具体的网址
+driver.get("http://baidu.com")
+#2、窗口最大化
+driver.maximize_window()
+#3、前进 后退 刷新
+driver.get("http://taobao.com")
+time.sleep(2) #等待=2s
+driver.forward()
+time.sleep(2)
+driver.back()
+time.sleep(2)
+driver.refresh()
+#4、关闭浏览器
+driver.close()#关闭当前的窗口
+driver.quit()#关闭整个浏览器
 ''''''
-#页面的标题
+# 页面的标题
 # driver.get("http://erp.lemfix.com/login.html")
 # title = driver.title #获取页面标题
 # print(title)
@@ -50,9 +50,9 @@ import requests
 # else:
 #     print("搜索失败！")
 
-url_baidu = "https://www.baidu.com/s"
-head = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"}
-data_baidu={"wd":"柠檬班"}
-resp = requests.get(url=url_baidu,headers=head,params=data_baidu)
-# print(resp.text) #文本结果 -- 自动解码 -- 80%
-print(resp.content.decode("utf8"))#手动指定解码方式
+# url_baidu = "https://www.baidu.com/s"
+# head = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0"}
+# data_baidu={"wd":"柠檬班"}
+# resp = requests.get(url=url_baidu,headers=head,params=data_baidu)
+# # print(resp.text) #文本结果 -- 自动解码 -- 80%
+# print(resp.content.decode("utf8"))#手动指定解码方式
